8eefc20bc07487e2d27aecc5f4e487b357c1c62a
forever <user@forever>
2026-05-14 21:08:06 +0300

    init commit

---
+++ account-service/src/test/java/ru/ugaforever/bank/account/AccountApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.ugaforever.bank.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
