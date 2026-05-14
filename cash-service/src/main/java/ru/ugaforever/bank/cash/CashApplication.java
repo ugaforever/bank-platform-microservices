feb02a12de9e6f74ccea822e0d880f6707aec7ae
forever <user@forever>
2026-05-14 21:44:47 +0300

    add cash-service, transfer-service, notification-service

---
+++ cash-service/src/main/java/ru/ugaforever/bank/cash/CashApplication.java
@@ -0,0 +1,11 @@
+package ru.ugaforever.bank.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CashApplication.class, args);
+    }
+}
