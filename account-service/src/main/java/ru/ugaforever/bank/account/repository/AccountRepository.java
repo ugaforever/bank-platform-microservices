35ccfbcb29270fcd276078047a939137e7252b65
forever <user@forever>
2026-05-17 11:40:26 +0300

    add repository tests to account-service

--- account-service/src/main/java/ru/ugaforever/bank/account/repository/AccountRepository.java
+++ account-service/src/main/java/ru/ugaforever/bank/account/repository/AccountRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.ugaforever.bank.account.model.Account;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByLogin(String login);
+
+    Optional<Account> findByName(String name);
 }
