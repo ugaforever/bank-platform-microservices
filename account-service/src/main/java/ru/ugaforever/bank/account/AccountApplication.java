437b4769e0010478e8d01a810b6857320198d9a8
forever <user@forever>
2026-05-17 18:07:37 +0300

    add Service Discovery (Consul) for gateway, account-service

--- account-service/src/main/java/ru/ugaforever/bank/account/AccountApplication.java
+++ account-service/src/main/java/ru/ugaforever/bank/account/AccountApplication.java
@@ -3,9 +3,10 @@ package ru.ugaforever.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
