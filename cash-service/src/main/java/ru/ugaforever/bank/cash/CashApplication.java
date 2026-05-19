2cdd6cb1e8a192b65f994a7a42b0fbe253bbfbf9
forever <user@forever>
2026-05-19 21:18:50 +0300

    add @EnableFeignClients

--- cash-service/src/main/java/ru/ugaforever/bank/cash/CashApplication.java
+++ cash-service/src/main/java/ru/ugaforever/bank/cash/CashApplication.java
@@ -3,9 +3,11 @@ package ru.ugaforever.bank.cash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CashApplication {
     public static void main(String[] args) {
         SpringApplication.run(CashApplication.class, args);
--- transfer-service/src/main/java/ru/ugaforever/bank/transfer/TransferApplication.java
+++ transfer-service/src/main/java/ru/ugaforever/bank/transfer/TransferApplication.java
@@ -4,9 +4,11 @@ package ru.ugaforever.bank.transfer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class TransferApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferApplication.class, args);
