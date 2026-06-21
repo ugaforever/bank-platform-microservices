8476bb3c4b992efa952b015e289920fefa6488a7
forever <user@forever>
2026-06-21 20:55:30 +0300

    update README.md

--- account-service/src/main/java/ru/ugaforever/bank/account/AccountApplication.java
+++ account-service/src/main/java/ru/ugaforever/bank/account/AccountApplication.java
@@ -4,10 +4,8 @@ package ru.ugaforever.bank.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import ru.ugaforever.bank.chassis.advice.GlobalExceptionHandler;
-import ru.ugaforever.bank.chassis.config.FeignConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
