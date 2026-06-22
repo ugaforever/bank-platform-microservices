de9a02c0424cd98f62bccb5cbf6d8ca90324a45c
forever <user@forever>
2026-06-22 18:49:11 +0300

    review 19/21: update context smoke tests (notification, account)

--- account-service/src/test/java/ru/ugaforever/bank/account/AccountApplicationTest.java
+++ account-service/src/test/java/ru/ugaforever/bank/account/AccountApplicationTest.java
@@ -2,11 +2,20 @@ package ru.ugaforever.bank.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.ugaforever.bank.account.repository.AccountRepository;
 
 @SpringBootTest
+@EmbeddedKafka(
+        topics = {"bank.notification", "bank.notification.dlt"},
+        partitions = 1,
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:0",
+                "auto.create.topics.enable=true"
+        }
+)
 @ActiveProfiles("test")
 class AccountApplicationTest {
 
@@ -15,5 +24,6 @@ class AccountApplicationTest {
 
     @Test
     void contextLoads() {
+
     }
 }
